b27c575fcccdfa8df8aa58f38a878d1876a812c8
wansongtao <18101837209@163.com>
2023-02-28 10:31:01 +0800

    refactor: 重构vue3组件注册方法

--- packages/utils/install.ts
+++ packages/utils/install.ts
@@ -2,9 +2,20 @@ import type { Plugin } from 'vue';
 
 export type SFCWithInstall<T> = T & Plugin;
 
-export const withInstall = <T = any>(main: SFCWithInstall<T>, name: string) => {
-  main.install = (app): void => {
-    app.component(name, main);
+export const withInstall = <T, E extends Record<string, any>>(
+  main: T,
+  extra?: E
+) => {
+  (main as SFCWithInstall<T>).install = (app): void => {
+    for (const comp of [main, ...Object.values(extra ?? {})]) {
+      app.component(comp.name, comp);
+    }
   };
-  return main;
+
+  if (extra) {
+    for (const [key, comp] of Object.entries(extra)) {
+      (main as any)[key] = comp;
+    }
+  }
+  return main as SFCWithInstall<T> & E;
 };
