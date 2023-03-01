6432c34404a12abaeb8d1adfcdad85bf3f73b4a8
wansongtao <18101837209@163.com>
2023-03-01 10:48:16 +0800

    chore: 工具包打包配置

---
+++ packages/utils/vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    dts({
+      outputDir: 'dist/es/',
+      //指定使用的tsconfig.json
+      tsConfigFilePath: '../../tsconfig.json'
+    }),
+    // //因为这个插件默认打包到es下，我们想让lib目录下也生成声明文件需要再配置一个
+    dts({
+      outputDir: 'dist/lib',
+      tsConfigFilePath: '../../tsconfig.json'
+    })
+  ],
+  build: {
+    target: 'modules',
+    lib: {
+      entry: resolve(__dirname, './index.ts')
+    },
+    rollupOptions: {
+      external: ['vue', 'ant-design-vue'],
+      output: [
+        {
+          format: 'es',
+          entryFileNames: '[name].js',
+          // preserveModules: true,
+          //配置打包根目录
+          dir: 'dist/es'
+        },
+        {
+          format: 'cjs',
+          entryFileNames: '[name].js',
+          // preserveModules: true,
+          //配置打包根目录
+          dir: 'dist/lib'
+        }
+      ]
+    }
+  }
+});
