9393a706883355c97cfd8cc5279e9f451e313122
wansongtao <18101837209@163.com>
2023-03-06 20:37:19 +0800

    perf: 移除没有用的的引入

--- packages/components/FileSelector/index.ts
+++ packages/components/FileSelector/index.ts
@@ -2,7 +2,6 @@ import FileSelector from './FileSelector.vue';
 import { withInstall } from '@stao-ui/utils';
 
 export type FileSelectorInstance = InstanceType<typeof FileSelector>;
-// export * from './FileSelector.vue';
 
 // 组件名
 FileSelector.name = 'STaoFileSelector';
