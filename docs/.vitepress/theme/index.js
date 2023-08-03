9e5f83376fa27c2c506efb37d652c3281d5acf1b
wansongtao <18101837209@163.com>
2023-08-03 10:33:37 +0800

    refactor: 取消vitepress全局注册组件

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,10 +1,5 @@
 import DefaultTheme from 'vitepress/theme';
-import { STaoButton, STaoFileSelector } from '@stao-ui/components';
 
 export default {
-  ...DefaultTheme,
-  enhanceApp({ app }) {
-    app.use(STaoButton); // 在 vitepress 中,注册全局组件
-    app.use(STaoFileSelector);
-  }
+  ...DefaultTheme
 };
