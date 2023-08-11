46f6f2cadc169912d4d41f74e40ea56e4674fae0
wansongtao <18101837209@163.com>
2023-08-11 14:29:51 +0800

    feat: 封装ajax方法

--- packages/utils/src/feature.ts
+++ packages/utils/src/feature.ts
@@ -147,3 +147,84 @@ export const aliOssImageResize = (
   return url;
 };
 // #endregion aliOssImageResize
+
+// #region ajax
+/**
+ * 使用XHR发送请求，获取数据
+ * @param param 
+ * @param beforeRequest 
+ * @returns 
+ */
+export const ajax = <T = unknown>(
+  param: {
+    url: string;
+    timeout?: number;
+    method?: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'OPTIONS' | 'HEAD' | 'PATCH';
+    data?: any;
+    params?: { [key: string]: string };
+    headers?: { [key: string]: string };
+    responseType?: XMLHttpRequestResponseType;
+  },
+  beforeRequest?: (xhr: XMLHttpRequest) => void
+): Promise<{ data: T; xhr: XMLHttpRequest }> => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status >= 200 && xhr.status < 300) {
+          resolve({ data: xhr.response, xhr });
+          return;
+        }
+
+        reject(new Error(`Request failed with status ${xhr.status}`));
+      }
+    };
+
+    if (beforeRequest) {
+      beforeRequest(xhr);
+    }
+
+    const {
+      url,
+      timeout = 5000,
+      method = 'GET',
+      headers = { 'Content-Type': 'application/json' },
+      data,
+      params,
+      responseType = 'json'
+    } = param;
+
+    if (params) {
+      const paramsStr = Object.keys(params)
+        .map((key) => `${key}=${params[key]}`)
+        .join('&');
+
+      xhr.open(method, `${url}?${paramsStr}`, true);
+    } else {
+      xhr.open(method, url, true);
+    }
+
+    xhr.responseType = responseType;
+    xhr.timeout = timeout;
+    Object.keys(headers).forEach((key) => {
+      xhr.setRequestHeader(key, headers[key]);
+    });
+
+    if (data && method !== 'GET') {
+      if (
+        headers['Content-Type'] === 'application/json' &&
+        typeof data === 'object'
+      ) {
+        xhr.send(JSON.stringify(data));
+        return;
+      }
+
+      xhr.send(data);
+      return;
+    }
+
+    xhr.send(null);
+  });
+};
+// #endregion ajax
