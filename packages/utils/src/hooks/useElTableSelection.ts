0d0f275537ca643dd9a8fb8ff942c12e8c88cb2b
wansongtao <18101837209@163.com>
2023-08-21 10:40:58 +0800

    style: el-table表格跨页多选hooks优化

--- packages/utils/src/hooks/useElTableSelection.ts
+++ packages/utils/src/hooks/useElTableSelection.ts
@@ -1,13 +1,27 @@
 import { ref, nextTick, type Ref } from 'vue';
 import { ElTable } from 'element-plus';
 
+/**
+ * @description el-table 跨页多选功能
+ * @param getPage 
+ * @param tableData 
+ * @param key 
+ * @returns 
+ */
 const useElTableSelection = <T = string | number>(
   getPage: () => number,
   tableData: Ref<any[]>,
   key = 'id'
-) => {
+): {
+  multipleTableRef: Ref<InstanceType<typeof ElTable> | undefined>;
+  multipleSelection: Ref<any[]>;
+  allPageSelection: T[][];
+  onSelectionChange: (val: any[]) => void;
+  revertSelection: () => void;
+  clearSelection: () => void;
+} => {
   let allPageSelection: T[][] = [];
-  const multipleSelection: Ref<T[]> = ref([]);
+  const multipleSelection: Ref<any[]> = ref([]);
   const onSelectionChange = (val: any[]) => {
     multipleSelection.value = val;
 
