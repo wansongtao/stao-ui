0af6d9675d8bf98e6e9dd9713a8897edb275b6e4
wansongtao <18101837209@163.com>
2023-10-30 10:19:29 +0800

    docs: 添加插入排序算法速度测试

---
+++ docs/algorithm/insertion-sort/sort.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+import BaseSort from '../component/baseSort.vue'
+import insertionSort from './insertionSort';
+</script>
+
+<template>
+  <base-sort :sort="insertionSort" />
+</template>
