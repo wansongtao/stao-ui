ac4f1f24d554b0ad0d23b85ea066540530466b96
wansongtao <18101837209@163.com>
2024-06-04 16:55:31 +0800

    refactor: 重构utils库的导出

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,21 +1,4 @@
 export * from './src/install';
 export * from './src/common';
 export * from './src/special';
-export * from './src/excel';
-export * from './src/downloadFile';
 export * from './src/event/eventBus';
-
-import useTextEllipsis from './src/hooks/useTextEllipsis';
-export { useTextEllipsis };
-
-import useResizeObserver from './src/hooks/useResizeObserver';
-export { useResizeObserver };
-
-import useIdleDetection from './src/hooks/useIdleDetection';
-export { useIdleDetection };
-
-import { useURLQuery, useURLQueryObject } from './src/hooks/useURLQuery';
-export { useURLQuery, useURLQueryObject };
-
-import { usePagingRequest } from './src/hooks/usePagingRequest';
-export { usePagingRequest };
