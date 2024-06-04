2d3a2fcb51656d940b2976bacd0a0724bfd67e31
wansongtao <18101837209@163.com>
2024-06-04 16:45:27 +0800

    style: 上传文件到阿里oss方法格式优化

--- packages/utils/src/api/upload.ts
+++ packages/utils/src/api/upload.ts
@@ -2,7 +2,7 @@ import axios, { AxiosProgressEvent } from 'axios';
 
 export interface IAliOssUploadRequestDTO {
   accessid: string;
-  callback: string;
+  callback?: string;
   dir: string;
   expire: string;
   host: string;
@@ -10,18 +10,6 @@ export interface IAliOssUploadRequestDTO {
   signature: string;
 }
 
-/**
- * @description 上传文件至阿里云oss接口
- * @param {Object} data
- * @param {string} data.policy 将过期时间和限制文件大小的对象加密后的字符串
- * @param {string} data.OSSAccessKeyId
- * @param {string} data.signature
- * @param {string} data.host 阿里云oss地址
- * @param {string} data.dir 文件存放路径
- * @param {string} [data.callback]
- * @param {File} file 二进制文件
- * @returns {Promise<string>} 成功返回 url
- */
 export const uploadAliOSS = (
   data: IAliOssUploadRequestDTO,
   file: File,
@@ -35,8 +23,10 @@ export const uploadAliOSS = (
   formData.append('OSSAccessKeyId', data.accessid);
   formData.append('signature', data.signature);
   formData.append('success_action_status', '200');
-  formData.append('callback', data.callback);
   formData.append('file', file);
+  if (data.callback) {
+    formData.append('callback', data.callback);
+  }
 
   return new Promise<string>((resolve, reject) => {
     axios({
