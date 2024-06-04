a4dd5c77504a1d99f1814daa1fb6f8d32d821f32
wansongtao <18101837209@163.com>
2024-06-04 16:39:48 +0800

    refactor: 移除日期面板组件

--- packages/components/src/DatePanel/index.ts
+++
@@ -1,6 +0,0 @@
-import DatePanel from './index.vue';
-import { withInstall } from '@stao-ui/utils';
-
-export type SDatePanelInstance = InstanceType<typeof DatePanel>;
-export const SDatePanel = withInstall(DatePanel);
-export default SDatePanel;
