8916492798d11516f325e32c5eb9d95a1adda01d
wansongtao <18101837209@163.com>
2024-06-05 14:12:00 +0800

    feat: 生成excel方法封装

--- packages/utils/src/plugins/downloadFile.ts
+++ packages/utils/src/plugins/downloadFile.ts
@@ -1,10 +1,5 @@
 import FileSaver from 'file-saver';
 
-/**
- * 下载二进制文件
- * @param data 
- * @param fileName 
- */
 export const downloadFile = (data: ArrayBuffer, fileName: string) => {
   const blob = new Blob([data], { type: 'application/octet-stream' });
   FileSaver.saveAs(blob, fileName);
