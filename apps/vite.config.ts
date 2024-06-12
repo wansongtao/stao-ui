12891a412260f3212c3ca8dd7eb23996de5c445e
wansongtao <18101837209@163.com>
2024-06-12 15:24:12 +0800

    chore: 测试环境修改ts配置

--- apps/vite.config.ts
+++ apps/vite.config.ts
@@ -8,7 +8,9 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('../packages/components/src', import.meta.url)),
+      '@utils': fileURLToPath(new URL('../packages/utils/src', import.meta.url))
     }
   },
   server: {
