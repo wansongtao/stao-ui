d2da0c52599e474a9262dfd401858d24f16db6ed
wansongtao <18101837209@163.com>
2024-06-17 11:18:39 +0800

    remove: 移除淘汰组件

--- packages/components/src/STabs/index.ts
+++
@@ -1,6 +0,0 @@
-import Tabs from './index.vue';
-import { withInstall } from '@stao-ui/utils';
-
-export type STabsInstance = InstanceType<typeof Tabs>;
-export const STabs = withInstall(Tabs);
-export default STabs;
