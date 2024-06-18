a55b75c2c0efde5959d7dbb0b604d11b95850742
wansongtao <18101837209@163.com>
2024-06-18 11:14:32 +0800

    remove: 移除文本滚动组件

--- packages/components/src/TextScroll/index.ts
+++
@@ -1,6 +0,0 @@
-import TextScroll from './index.vue';
-import { withInstall } from '@stao-ui/utils';
-
-export type TextScrollInstance = InstanceType<typeof TextScroll>;
-export const STextScroll = withInstall(TextScroll);
-export default STextScroll;
