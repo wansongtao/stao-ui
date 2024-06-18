c79e273c25aa6fe4bfb0dee0487d66c783df551b
wansongtao <18101837209@163.com>
2024-06-18 14:05:15 +0800

    refactor: 组件重命名

--- packages/components/src/ScrollList/index.ts
+++
@@ -1,6 +0,0 @@
-import ScrollListVue from "./ScrollList.vue";
-import { withInstall } from '@stao-ui/utils';
-
-export type ScrollListInstance = InstanceType<typeof ScrollListVue>;
-export const ScrollList = withInstall(ScrollListVue);
-export default ScrollList;
