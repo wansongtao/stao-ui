7fcecf65472681bd5b96c3afff904f7613f08520
wansongtao <18101837209@163.com>
2024-06-20 16:06:44 +0800

    remove: 移除多余组件

--- packages/components/src/TextEllipsis/index.ts
+++
@@ -1,6 +0,0 @@
-import TextEllipsis from './index.vue';
-import { withInstall } from '@stao-ui/utils';
-
-export type TextEllipsisInstance = InstanceType<typeof TextEllipsis>;
-export const STextEllipsis = withInstall(TextEllipsis);
-export default STextEllipsis;
