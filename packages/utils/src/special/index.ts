878a11d3014487d14e84e9c2ab142c022f5c9987
wansongtao <18101837209@163.com>
2025-11-23 20:05:53 +0800

    refactor: 重构深拷贝方法

--- packages/utils/src/special/index.ts
+++ packages/utils/src/special/index.ts
@@ -381,3 +381,90 @@ export const fillArray = <T = number>(
   return arr;
 };
 // #endregion fillArray
+
+
+/**
+ * 深拷贝实现(支持Map、Set、RegExp、Date、Function类型和循环引用，不支持symbol属性)
+ * 优化版本：使用WeakMap解决循环引用，减少类型检测次数，提升性能
+ */
+export const deepClone = <T = unknown>(obj: T): T => {
+  // 使用 WeakMap 解决循环引用问题，避免内存泄漏
+  const visited = new WeakMap()
+
+  const clone = (data: any): any => {
+    if (data === null || typeof data !== 'object') {
+      return data
+    }
+
+    if (visited.has(data)) {
+      return visited.get(data)
+    }
+
+    const type = Object.prototype.toString.call(data)
+    let result: any
+    switch (type) {
+      case '[object Date]':
+        result = new Date(data.getTime())
+        break
+
+      case '[object RegExp]':
+        result = new RegExp(data.source, data.flags)
+        break
+
+      case '[object Function]':
+        try {
+          result = data.bind({})
+        } catch {
+          // 降级方案：对于箭头函数等特殊情况
+          const funcStr = data.toString()
+          if (funcStr.startsWith('function')) {
+            result = new Function(`return ${funcStr}`)()
+          } else {
+            result = new Function(`return function ${funcStr.replace(/^function/, '')}`)()
+          }
+        }
+        break
+
+      case '[object Map]':
+        result = new Map()
+        visited.set(data, result)
+        data.forEach((val: any, key: any) => {
+          result.set(clone(key), clone(val))
+        })
+        return result
+
+      case '[object Set]':
+        result = new Set()
+        visited.set(data, result)
+        data.forEach((val: any) => {
+          result.add(clone(val))
+        })
+        return result
+
+      case '[object Array]':
+        result = []
+        visited.set(data, result)
+        for (let i = 0; i < data.length; i++) {
+          result[i] = clone(data[i])
+        }
+        return result
+
+      default:
+        result = {}
+        visited.set(data, result)
+
+        // 使用 Object.keys 而不是 for...in，性能更好
+        const keys = Object.keys(data)
+        for (let i = 0; i < keys.length; i++) {
+          const key = keys[i]
+          result[key] = clone(data[key])
+        }
+        return result
+    }
+
+    visited.set(data, result)
+    return result
+  }
+
+  return clone(obj)
+}
